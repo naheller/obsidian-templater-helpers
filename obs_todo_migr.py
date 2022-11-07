019d0fb8eeda88ffb993509a73cc026e653a3136
Nathan Heller <nathan@alloy.com>
2022-11-06 19:20:00 -0500

    initial customizations

--- obs_todo_migr.py
+++ obs_todo_migr.py
@@ -9,10 +9,9 @@ from re import match
 # supply path via Templater user function definition: <path>/obs_todo_migr.py <% tp.file.folder(true) %>
 daily_notes_dir = sys.argv[1]
 
-unchecked = r"- \[ \]"
 prevnote = []
 open_todos = []
-carryover_tally = "." # append each time todo is migrated
+unchecked = "- [ ]"
 
 files = [f for f in listdir(daily_notes_dir) if isfile(join(daily_notes_dir, f))]
 
@@ -20,24 +19,21 @@ if len(files) >= 2:
     files.sort(reverse=True)
     prevfile = files[1] # need second newest file since new note exists when this is called
 
-    # avoid race by waiting for the obs_dates_nav script which may be invoked at same time
-    sleep(2)
+    # grab unchecked todos to move to new day
+    start_reading = False
 
-    # grab unchecked todos to move to new day, remove from previous day's note
-    with open(daily_notes_dir+"/"+prevfile, 'r+') as f:
+    with open(daily_notes_dir+"/"+prevfile, 'r') as f:
         lines = f.readlines()
         for line in lines:
-            if match(unchecked, line):
+            if '***Today***' in line:
+                start_reading = True
+                continue
+            if start_reading and line.strip() is "":
+                break
+            if start_reading and unchecked in line:
                 open_todos.append(line)
-            else:
-                prevnote.append(line)
-        f.seek(0)
-        for line in prevnote:
-            f.write(line)
-        f.truncate()
 
-    # Templater plugin completes macro from sdout
     for todo in open_todos:
-        print(todo[:-1] + carryover_tally)
+        print(todo[:-1])
 else:
     print('- [ ]')
